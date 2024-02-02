28d0e0981a7178d89ad82b5fb142a6b8d95657db
arch1na <155053978+arch1na@users.noreply.github.com>
2024-02-02 12:04:14 +0530

    Add files via upload

---
+++ skincarefoodd.dart
@@ -0,0 +1,323 @@
+import 'package:flutter/material.dart';
+import 'package:samplecode/SKINCAREF1.dart';
+import 'package:samplecode/SKINCAREF2.dart';
+import 'package:samplecode/SKINCAREF3.dart';
+import 'package:samplecode/SKINCAREF4.dart';
+import 'package:samplecode/SKINCAREF5.dart';
+import 'package:samplecode/SKINCAREF6.dart';
+class skincarefoodd extends StatefulWidget {
+  const skincarefoodd({super.key});
+
+  @override
+  State<skincarefoodd> createState() => _skincarefooddState();
+}
+
+class _skincarefooddState extends State<skincarefoodd> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF2DFB2),
+      appBar:
+      AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Color(0xffF2DFB2),
+        elevation: 0.0,
+        title: Row(
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(top:20.0),
+                child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back_ios_rounded,
+                  size: 30,color: Color(0xff7A5600),))
+            ),
+
+            Padding(padding: EdgeInsets.only(left: 50.0, top: 8.0)),
+            Image.asset('assets/images/logo.png'),
+            InkWell(
+                onTap: (){},child: Ink(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 30.0,top: 20),
+                child: CircleAvatar(
+                  radius: 25,
+                  backgroundImage: AssetImage("assets/images/profile.png",
+                  ),
+                ),
+              ),
+            )
+            )
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 26, right: 30),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(40),
+                      borderSide: BorderSide(color: Color(0xffd6d6d6)),
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Search shop, sitters or etc',
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 20.0,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.menu,
+                      size: 20.0,
+                    )
+                ),
+              ),
+            ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.only(right: 100),
+              child: Text('Skin Care Foods',style: TextStyle(
+                fontFamily: 'Katibeh',
+                color: Color(0xff604401),
+                fontSize: 40,
+              ),),
+            ),
+            SizedBox(height: 10,),
+            Row(
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF1()));
+                  },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage('assets/images/back.png'))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20,left: 10,top: 10,right: 10),
+                    child: Image.asset('assets/images/skin1.png'),
+                  ),
+                ),
+                ),
+                InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF2()));
+                  },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/back.png'))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30,left: 10,top: 20,right: 10),
+                    child: Image.asset('assets/images/skin2.png'),
+                  ),
+                ),
+                ),
+                InkWell(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF3()));
+                    },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/back.png'))
+                  ),
+                    child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30,left: 10,top: 20,right: 10),
+                    child: Image.asset('assets/images/skin3.png'),
+                  ),
+                )
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      new Text('Himalaya\nHealthy\nPowder\nPet Food,\nMeat & Rice,\nAdult Dog,\n10 Kg',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: new Text('₹1,685',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Column(
+                    children: [
+                      new Text('Farmina Pet\nFoods Quinoa\nSkin and Coat\nDry Dog Food,\nGrain-Free,\nAdult Breed,\n2.5-kg',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 60),
+                        child: new Text('₹2,710',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      new Text('Purina Pro\nPlan Puppy\nDry Dog Food\nfor Medium\nBreed,Chicken\nFlavor2.5kg',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: new Text('₹1,802',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40,),
+            Row(
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF4()));
+                  },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage('assets/images/back.png'))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30,left: 10,top: 20,right: 10),
+                    child: Image.asset('assets/images/skin4.png'),
+                  ),
+                ),
+                ),
+                InkWell(
+                  onTap: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF5()));
+                  },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/back.png'))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30,left: 20,top: 20,right: 10),
+                    child: Image.asset('assets/images/skin5.png'),
+                  ),
+                ),
+                ),
+                InkWell(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => SKINCAREF6()));
+                    },child: Ink(
+                  height:120,
+                  width: 120,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/back.png'))
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20,left: 10,top: 10,right: 10),
+                    child: Image.asset('assets/images/skin6.png'),
+                  ),
+                )
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      new Text('Purina\nSupercoat\nAdult Healthy\nWeight Dry\nDog Food,\nChicken,\n3 Kg',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 40),
+                        child: new Text('₹1,012',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      new Text('Vet Solution\nCanine-\nDermatosis\n2kg (Dietetic\nFood for\nDogs Skin)',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: new Text('₹2,300',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 40),
+                  child: Column(
+                    children: [
+                      new Text('Himalaya\nHealthy\nPet Food\nFor Dog -\nPuppy,Large,\nChicken,\n3 kg',
+                        style: TextStyle(color: Color(0xff604401),
+                            fontFamily: 'Inter',
+                            fontSize: 14),),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 50),
+                        child: new Text('₹770',textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontFamily:'Inter'
+                          ),),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
